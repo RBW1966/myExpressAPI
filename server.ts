87fb7f1e29c16bf3c39065790909f3891e2f3b70
Rob <rob_weed@hotmail.com>
2018-10-14 00:27:55 -0700

    more

--- server.ts
+++ server.ts
@@ -50,60 +50,31 @@ const checkJwt = jwt({
     cache: true,
     rateLimit: true,
     jwksRequestsPerMinute: 5,
-    jwksUri: `https://<YOUR_AUTH0_DOMAIN>/.well-known/jwks.json`
+    jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
   }),
 
   // Validate the audience and the issuer.
-  audience: '<YOUR_AUTH0_CLIENT_ID>',
-  issuer: `https://<YOUR_AUTH0_DOMAIN>/`,
+  audience: `${process.env.AUTH0_CLIENT_ID}`,
+  issuer: `https://${process.env.AUTH0_DOMAIN}/`,
   algorithms: ['RS256']
 });
 
-// insert a new question
-app.post('/', checkJwt, (req, res) => {
-  const {title, description} = req.body;
-  const newQuestion = {
-    id: questions.length + 1,
-    title,
-    description,
-    answers: [],
-    author: req.user.name,
-  };
-  questions.push(newQuestion);
-  res.status(200).send();
-});
-
-// insert a new answer to a question
-app.post('/answer/:id', checkJwt, (req, res) => {
-  const {answer} = req.body;
-
-  const question = questions.filter(q => (q.id === parseInt(req.params.id)));
-  if (question.length > 1) return res.status(500).send();
-  if (question.length === 0) return res.status(404).send();
-
-  question[0].answers.push({
-    answer,
-    author: req.user.name,
-  });
-
-  res.status(200).send();
-});
-
 // // insert a new question
-// app.post('/', (req, res) => {
+// app.post('/', checkJwt, (req, res) => {
 //   const {title, description} = req.body;
 //   const newQuestion = {
 //     id: questions.length + 1,
 //     title,
 //     description,
 //     answers: [],
+//     author: req.user.name,
 //   };
 //   questions.push(newQuestion);
 //   res.status(200).send();
 // });
 
 // // insert a new answer to a question
-// app.post('/answer/:id', (req, res) => {
+// app.post('/answer/:id', checkJwt, (req, res) => {
 //   const {answer} = req.body;
 
 //   const question = questions.filter(q => (q.id === parseInt(req.params.id)));
@@ -112,10 +83,39 @@ app.post('/answer/:id', checkJwt, (req, res) => {
 
 //   question[0].answers.push({
 //     answer,
+//     author: req.user.name,
 //   });
 
 //   res.status(200).send();
 // });
+
+// insert a new question
+app.post('/', (req, res) => {
+  const {title, description} = req.body;
+  const newQuestion = {
+    id: questions.length + 1,
+    title,
+    description,
+    answers: [],
+  };
+  questions.push(newQuestion);
+  res.status(200).send();
+});
+
+// insert a new answer to a question
+app.post('/answer/:id', (req, res) => {
+  const {answer} = req.body;
+
+  const question = questions.filter(q => (q.id === parseInt(req.params.id)));
+  if (question.length > 1) return res.status(500).send();
+  if (question.length === 0) return res.status(404).send();
+
+  question[0].answers.push({
+    answer,
+  });
+
+  res.status(200).send();
+});
 
 // start the server
 const port = normalizePort(process.env.PORT);
