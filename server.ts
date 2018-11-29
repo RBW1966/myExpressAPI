f8000e563485696dc5f7f5bc7112a46c3797199e
Rob Weed <rob_weed@hotmail.com>
2018-11-28 21:50:14 -0800

    crypto

--- server.ts
+++ server.ts
@@ -1,3 +1,29 @@
+// Nodejs encryption with CTR
+const crypto = require('crypto'),
+  algorithm = 'aes-256-ctr',
+  password = 'd6F3Efeq';
+
+function encrypt(text){
+  var cipher = crypto.createCipher(algorithm,password)
+  var crypted = cipher.update(text,'utf8','hex')
+  crypted += cipher.final('hex');
+  return crypted;
+}
+     
+function decrypt(text){
+  var decipher = crypto.createDecipher(algorithm,password)
+  var dec = decipher.update(text,'hex','utf8')
+  dec += decipher.final('utf8');
+  return dec;
+}
+     
+    var hw = encrypt("hello world")
+    // outputs hello world
+    console.log(decrypt(hw));
+
+/* -----
+/
+*/
 import { json } from "express";
 
 //import dependencies
